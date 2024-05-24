ed4b523f01c1e64606710834c459ebf6750bc986
Joshgun <143557263+joshgun-ab@users.noreply.github.com>
2024-05-24 15:46:05 +0400

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
+#include <stdio.h>
+
 int main()
 {
   int i;
-  for (i = 0; i < 10; i++)
-  {
-    printf("School\n");
+  for (i = 0; i < 32; i++) // 
+    printf("Hello School\n"); //
   }
   return 0;
 }
+
